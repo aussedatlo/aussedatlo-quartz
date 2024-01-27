6452e0881e21c4f49d6fddd0981391fe28753255
Louis Aussedat <aussedat.louis@gmail.com>
2024-01-27 21:00:05 +0100

    add favicon

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -12,8 +12,7 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
-    const iconPath = joinSegments(baseDir, "static/icon.png")
-    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
+    const iconPath = joinSegments(baseDir, "static/favicon.ico")
 
     return (
       <head>
@@ -22,7 +21,6 @@ export default (() => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="675" />
         <link rel="icon" href={iconPath} />
